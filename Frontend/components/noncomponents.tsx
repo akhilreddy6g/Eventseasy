cefa950003f5385d3664c3735a62a84d822b96f4
akhilreddy6g <akhilreddyoff21@gmail.com>
2025-06-17 20:11:27 -0400

    Update Frontend Dependencies

--- Frontend/components/noncomponents.tsx
+++ Frontend/components/noncomponents.tsx
@@ -1,14 +1,13 @@
 import axios from "axios";
 
 export const apiUrl = axios.create({
-  baseURL: "http://localhost:4000",
-  // baseURL: "/api",
+  baseURL: "http://localhost:4000/api",
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
 
 export const apiServerUrl = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "http://localhost:4000/api",
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
