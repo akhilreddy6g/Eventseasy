ca905972ad0e3d7dbc6aed3a4a500be92c382296
akhilreddy6g <akhilreddyoff21@gmail.com>
2025-07-07 16:33:17 -0400

    Delete Next middleware

--- Frontend/app/(dashboard)/layout.tsx
+++ Frontend/app/(dashboard)/layout.tsx
@@ -1,11 +1,19 @@
 import { DashboardHeader } from "@/components/dashboard/header"
 import { DashboardSidebar } from "@/components/dashboard/sidebar"
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const accessToken = cookies().get("accessToken")
+  console.log("other cookies are:", cookies().getAll())
+  console.log("accessToken in dashboard layout is:", accessToken)
+  if (!accessToken) {
+    redirect("/") 
+  }
   return (
     <div className="min-h-screen flex">
       <DashboardSidebar />
