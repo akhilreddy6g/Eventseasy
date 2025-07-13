d2810ae36ad057e731ff294b8a13f51cab45458d
akhilreddy6g <akhilreddyoff21@gmail.com>
2025-07-13 02:35:59 -0400

    Set only accessToken cookie on client side

--- Frontend/components/actions/auth.ts
+++ Frontend/components/actions/auth.ts
@@ -15,35 +15,29 @@ export async function authLoginServerAction(formData: FormData) {
     const res = await serverApi.post(route, data);
     let accessToken
     const setCookies = res.headers["set-cookie"] || [];
-    for (const raw of setCookies) {
-      const [cookieStr, ...attrs] = raw.split("; ");
-      const [name, value] = cookieStr.split("=");
-      const options: Record<string, any> = {};
-      for (const attr of attrs) {
-        const [key, val] = attr.split("=");
-        const k = key.toLowerCase();
-        if (k === "secure") options.secure = true;
-        else if (k === "httponly") options.httpOnly = true;
-        else if (k === "samesite") options.sameSite = val;
-        else if (k === "max-age") options.maxAge = parseInt(val, 10);
-        else if (k === "path") options.path = val;
-        else if (k === "expires") options.expires = new Date(val);
+    const accessTokenRaw = setCookies.find((c: string) =>
+        c.startsWith("accessToken=")
+      );
+      if (accessTokenRaw) {
+        const [cookieStr, ...attrs] = accessTokenRaw.split("; ");
+        const [name, value] = cookieStr.split("=");
+        const options: Record<string, any> = {};
+        for (const attr of attrs) {
+          const [key, val] = attr.split("=");
+          const k = key.toLowerCase();
+          if (k === "secure") options.secure = true;
+          else if (k === "httponly") options.httpOnly = true;
+          else if (k === "samesite") options.sameSite = val;
+          else if (k === "max-age") options.maxAge = parseInt(val, 10);
+          else if (k === "path") options.path = val;
+          else if (k === "expires") options.expires = new Date(val);
+        }
+        cookies().set(name, decodeURIComponent(value), options);
+        const parsedAccessToken = JSON.parse(decodeURIComponent(value))?.act;
+        return {success: res.data.success, user: res.data.user, userName: res.data.userName, accessToken: parsedAccessToken};
       }
-      cookies().set(name, decodeURIComponent(value), options);
-      accessToken = cookies().get("accessToken")?.value;
-      if (accessToken) {
-      const authorization = JSON.parse(decodeURIComponent(accessToken))?.act;
-      accessToken = authorization
-      }
-    }
-
-    return {
-      success: res.data.success,
-      user: res.data.user,
-      userName: res.data.userName,
-      accessToken: accessToken
-    };
-  } catch (err) {
+      return { success: false, error: "No accessToken found" };
+    }catch (err) {
     console.error("Server action error:", err);
     return { success: false };
   }
