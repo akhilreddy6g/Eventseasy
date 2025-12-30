cbe93ca56c6d1f0ea0a7f3e5f177ec90d29c04e4
akhilreddy6g <akhilreddyoff21@gmail.com>
2025-12-30 12:36:44 -0500

    Delete unused code

--- kafka_pubsub/kafka-client.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.kafka = void 0;
-// kafkaClient.ts
-var kafkajs_1 = require("kafkajs");
-exports.kafka = new kafkajs_1.Kafka({
-    clientId: 'my-app',
-    brokers: ['localhost:9092'],
-});
